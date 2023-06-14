f24cd6b439c5281729eed229cf721cc041b82728
User <alannariya@gmail.com>
2023-06-14 17:56:21 +0200

    commit2

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -1,26 +1,15 @@
-
 import React, { Fragment } from 'react';
 
-  
-function withRainbowFrame(colors) {
-  return function (Component) {
-    class ComponentWithRainbowFrame extends React.Component {
-      render() {
-        let content = <Component {...this.props} />;
-        for (let color of colors) {
-          content = (
-            <div
-              className='RainbowFrame'
-              style={{ border: `solid 5px ${color}`, padding: '10px' }}
-            >
-              {content}
-            </div>
-          );
-        }
-        return <Fragment>{content}</Fragment>;
-      }
-    }
-    return ComponentWithRainbowFrame;
-  };
-}
+const withRainbowFrame = (colors) => (Component) => (props) => {
+  let content = <Component {...props} />;
+  for (let color of colors) {
+    content = (
+      <div className='RainbowFrame' style={{ border: `solid 5px ${color}`, padding: '10px' }}>
+        {content}
+      </div>
+    );
+  }
+  return <Fragment>{content}</Fragment>;
+};
+
 export default withRainbowFrame;
